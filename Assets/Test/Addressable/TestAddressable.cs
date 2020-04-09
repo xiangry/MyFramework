712feb965f9e97edefd7701f4283f93bb577d60b
xsg2019 <xingsheng.gao@kaiqigu.com>
2020-04-09 21:04:17 +0800

    添加Addressable

---
+++ Assets/Test/Addressable/TestAddressable.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public class TestAddressable : MonoBehaviour
+{
+    private Canvas uiRoot;
+
+    private AsyncOperationHandle<GameObject> handle;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiRoot = GameObject.Find("Canvas").GetComponent<Canvas>();
+        
+        DontDestroyOnLoad(gameObject);
+        
+        DoTest();
+    }
+
+    void DoTest()
+    {
+        Debug.Log($"DoTest -------------");
+        handle = Addressables.LoadAssetAsync<GameObject>("test");
+        handle.Completed += OnResLoadedHandler;
+    }
+
+    void OnResLoadedHandler(AsyncOperationHandle<GameObject> obj)
+    {
+        Debug.Log($"OnResLoadedHandler Status:{obj.Status} Result:{obj.Result}");
+        if (obj.Result)
+        {
+            GameObject go = obj.Result;
+            GameObject uiloading = Instantiate(go, uiRoot.transform);
+        }
+
+    }
+}
---
+++ Assets/Test/TestSwipe/TestSwipe.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using HedgehogTeam.EasyTouch;
+using UnityEngine;
+
+public class TestSwipe : MonoBehaviour
+{
+    private bool isSwipe = false;
+    private EasyTouch.SwipeDirection lastSwipe = EasyTouch.SwipeDirection.None;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        EasyTouch.On_SwipeStart += OnSwipeStart;
+        EasyTouch.On_Swipe += OnSwipe;
+        EasyTouch.On_SwipeEnd += OnSwipeEnd;
+    }
+
+    void OnSwipeStart(Gesture gesture)
+    {
+        Debug.Log($"OnSwipeStart swipe:{gesture.swipe} Length:{gesture.swipeLength} swipeVector:{gesture.swipeVector} twistAngle:{gesture.twistAngle}");
+    }
+
+    void OnSwipe(Gesture gesture)
+    {
+        isSwipe = true;
+        if (gesture.swipe == EasyTouch.SwipeDirection.Other)
+        {
+            return;
+        }
+
+        if (gesture.swipe != lastSwipe)
+        {
+            Debug.Log($"滑动方向 {gesture.swipe}");
+            lastSwipe = gesture.swipe;
+        }
+    }
+    
+    void OnSwipeEnd(Gesture gesture)
+    {
+        lastSwipe = EasyTouch.SwipeDirection.None;
+        isSwipe = false;
+        Debug.Log($"OnSwipeEnd swipe:{gesture.swipe} Length:{gesture.swipeLength} swipeVector:{gesture.swipeVector} twistAngle:{gesture.twistAngle}");
+    }
+    
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
